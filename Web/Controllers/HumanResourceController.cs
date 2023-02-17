cc34d52184fd2506fe5117e46378d7f6ed3310d2
kutluhanyegit <kutluhanyegit@icloud.com>
2023-02-17 10:11:01 +0300

    v1.0.10

--- Web/Controllers/HumanResourceController.cs
+++ Web/Controllers/HumanResourceController.cs
@@ -5,38 +5,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using Web.Models;
 
 namespace Web.Controllers
 {
-    
-    [Route("giris")]
-    public class LoginController : Controller
+        
+    [Route("insan-kaynaklari")]
+    public class HumanResourceController : Controller
     {
-        private readonly ILogger<LoginController> _logger;
+        private readonly ILogger<HumanResourceController> _logger;
 
-        public LoginController(ILogger<LoginController> logger)
+        
+        public HumanResourceController(ILogger<HumanResourceController> logger)
         {
             _logger = logger;
         }
 
         [Route("")]
-        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
-        
-        
-        [Route("")]
-        [HttpPost]
-        public IActionResult Index(LoginViewModel loginvm)
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        [Route("error")]
         public IActionResult Error()
         {
             return View("Error!");
