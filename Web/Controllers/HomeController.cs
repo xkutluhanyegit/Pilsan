8410206c22fa03cb0d6c676ac52871b4f292e279
kutluhanyegit <kutluhanyegit@icloud.com>
2023-02-17 11:15:21 +0300

    v1.0.11

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        List<LoginViewModel> get = new List<LoginViewModel>();
         return View();
     }
 
--- Web/Controllers/LoginController.cs
+++ Web/Controllers/LoginController.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Web.Models;
 
 namespace Web.Controllers
 {
-    
+
     [Route("giris")]
     public class LoginController : Controller
     {
