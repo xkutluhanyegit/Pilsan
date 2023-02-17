74c82507c6a7aa1d555bf80bd5bc3f37f84df93b
kutluhanyegit <kutluhanyegit@icloud.com>
2023-02-17 16:56:50 +0300

    v1.0.12

--- Entities/Concrete/Test.cs
+++ Entities/Concrete/Test.cs
@@ -8,6 +8,9 @@ namespace Entities.Concrete
 {
     public class Test:IEntity
     {
+        public int TestID { get; set; }
+        public string PersonelName { get; set; }
+        public string PersonelDepartment { get; set; }
         
     }
 }
